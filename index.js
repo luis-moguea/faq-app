10155c17ad65f13bb738bd8e24e263149111aa6a
luis-moguea <luis.mogueadp@gmail.com>
2022-12-13 15:47:11 -0500

    html updates

---
+++ index.js
@@ -0,0 +1,14 @@
+const list = document.querySelector("#list")
+
+const element = /*html*/`
+
+<div class="main" id="list">
+<ul>
+    <div>
+        <p></p>
+    </div>
+    <li>
+        
+    </li>
+</ul>
+</div>`
