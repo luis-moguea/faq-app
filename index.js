db7246b919ebcc444989749ff0eff5d80c4c4bae
luis-moguea <luis.mogueadp@gmail.com>
2022-12-15 18:22:06 -0500

    js and style updates

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
-const row = document.querySelector("#row")
-const answer = document.querySelector("#answer")
-const answerTwo = document.querySelector("#answer-1")
+const row = document.querySelectorAll(".row")
+const answers = document.querySelectorAll(".main__list--faq")
+const answerText = document.querySelectorAll(".answers")
 
 
-    row.addEventListener("click", () => {
-        answer.classList.toggle("text")
-        row.classList.toggle("row-rotate")
-    })
+    for(let i = 0; i < answers.length; i++){
+        answers[i].addEventListener("click", () => {
+            answerText[i].classList.toggle("active")
+            row[i].classList.toggle("row-rotate")
+        })
+    }
+
 
 
 
