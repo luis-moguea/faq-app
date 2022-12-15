7a730085e3fa6cde4bf5ce2a850ca77eaa9d7dee
luis-moguea <luis.mogueadp@gmail.com>
2022-12-14 23:18:39 -0500

    js updates

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-const list = document.querySelector("#list")
+const row = document.querySelector("#row")
+const answer = document.querySelector("#answer")
+const answerTwo = document.querySelector("#answer-1")
+
+
+    row.addEventListener("click", () => {
+        answer.classList.toggle("text")
+        row.classList.toggle("row-rotate")
+    })
+
+
+
