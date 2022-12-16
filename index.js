d57fe43647178f2004a34df0169cdf57fedec112
luis-moguea <luis.mogueadp@gmail.com>
2022-12-16 00:13:55 -0500

    js and style updates

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const row = document.querySelectorAll(".row")
 const answers = document.querySelectorAll(".main__list--faq")
 const answerText = document.querySelectorAll(".answers")
+const questions = document.querySelectorAll(".questions__text")
 
 
     for(let i = 0; i < answers.length; i++){
         answers[i].addEventListener("click", () => {
             answerText[i].classList.toggle("active")
             row[i].classList.toggle("row-rotate")
+            questions[i].classList.toggle("text-bold")
         })
     }
 
