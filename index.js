9f69cbf25f6dd5c8db8ab2f6d60722ca48e90af9
luis-moguea <luis.mogueadp@gmail.com>
2022-12-19 19:11:52 -0500

    js loop update

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
-const row = document.querySelectorAll(".row")
-const answers = document.querySelectorAll(".main__list--faq")
+const arrow = document.querySelectorAll(".row")
+const item = document.querySelectorAll(".main__list--faq")
 const answerText = document.querySelectorAll(".answers")
 const questions = document.querySelectorAll(".questions__text")
 
 
-    for(let i = 0; i < answers.length; i++){
-        answers[i].addEventListener("click", () => {
+    /*for(let i = 0; i < answers.length; i++){
+        item[i].addEventListener("click", () => {
             answerText[i].classList.toggle("active")
-            row[i].classList.toggle("row-rotate")
+            arrow[i].classList.toggle("row-rotate")
             questions[i].classList.toggle("text-bold")
         })
-    }
+    }*/
 
 
-
+    item.forEach(el => {
+        el.addEventListener("click", () => {
+            el.querySelector(".answers").classList.toggle("active")
+            el.querySelector(".row").classList.toggle("row-rotate")
+            el.querySelector(".questions__text").classList.toggle("text-bold")
+        })
+    });
